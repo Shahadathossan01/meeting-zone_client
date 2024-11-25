850b67bc66e87b30cfe3669b3ebc20dfaaed5f9a
shahadat hossen <hossantopu@gmail.com>
2024-11-26 04:31:36 +0600

    complete bookingForm design

--- src/pages/BookingForm/BookingForm.jsx
+++ src/pages/BookingForm/BookingForm.jsx
@@ -17,19 +17,20 @@ const BookingForm = () => {
 
     return (
         <div>
-            <h1>Wishing for the best meetup</h1>
+            <h1 className="text-center pt-6 font-serif hover:font-mono text-2xl">Wishing for the best meetup!</h1>
+            <div className="flex justify-center mt-6">
             <form onSubmit={handleSubmit(onSubmit)}>
-                <div>
-                    <label htmlFor="username">Username</label><br />
-                    <input {...register("username")} placeholder="enter you username" className="border" type="text" name="username" id="username" />
+                <div className="mb-4">
+                    <label className="text-2xl text-bold" htmlFor="username">Username:</label><br />
+                    <input required {...register("username")} placeholder="enter you username" className="border-2 px-2" type="text" name="username" id="username" />
                 </div>
-                <div>
-                    <label htmlFor="location">Location</label><br />
-                    <input disabled className="border" placeholder={itemLocation} type="text" name="location" id="" />
+                <div className="mb-4">
+                    <label className="text-2xl text-bold" htmlFor="location">Location</label><br />
+                    <input disabled className="border-2 px-2" placeholder={itemLocation} type="text" name="location" id="" />
                 </div>
-                <div>
-                    <label htmlFor="meetupType">Meetup Type</label><br />
-                    <select {...register("meetupType")} className="border" name="meetupType" id="meetupType">
+                <div className="mb-4">
+                    <label className="text-2xl text-bold" htmlFor="meetupType">Meetup Type</label><br />
+                    <select required {...register("meetupType")} className="border-2 px-2" name="meetupType" id="meetupType">
                         <option value="Education">Education</option>
                         <option value="Business">Business</option>
                         <option value="Entertainment">Entertainment</option>
@@ -37,32 +38,35 @@ const BookingForm = () => {
                         <option value="Get Together">Get Together</option>
                     </select>
                 </div>
-                <div>
-                    <label htmlFor="date">Date</label><br />
-                    <input {...register("date")} className="border" type="date" name="date" id="date" />
+                <div className="mb-4">
+                    <label className="text-2xl text-bold" htmlFor="date">Date</label><br />
+                    <input required {...register("date")} className="border-2 px-2" type="date" name="date" id="date" />
                 </div>
-                <div>
-                    <label htmlFor="shift">Shift</label><br />
-                    <select {...register("shift")} className="border" name="shift" id="shift">
+                <div className="mb-4">
+                    <label className="text-2xl text-bold" htmlFor="shift">Shift</label><br />
+                    <select required {...register("shift")} className="border-2 px-2" name="shift" id="shift">
                         <option value="Day">Day</option>
                         <option value="Night">Night</option>
                     </select>
                 </div>
-                <div>
-                    <label htmlFor="members">Members</label><br />
-                    <input {...register("members")} className="border" type="number" name="members" id="members" />
+                <div className="mb-4">
+                    <label className="text-2xl text-bold" htmlFor="members">Members</label><br />
+                    <input required {...register("members")} className="border-2 px-2" type="number" name="members" id="members" />
                 </div>
                 <div>
-                    <label htmlFor="duration">Duration <span>(hour)</span></label><br />
-                    <input {...register("duration")} className="border" type="number" name="duration" id="duration" />
+                    <label className="text-2xl text-bold" htmlFor="duration">Duration <span>(hour)</span></label><br />
+                    <input required {...register("duration")} className="border-2 px-2" type="number" name="duration" id="duration" />
                 </div>
                 <div>
                     <label htmlFor="cost">Cost: 1000 taka <span>(per hour)</span></label>
                 </div>
                 <hr />
-                <h1>Total Cost:{1000*(duration>=0?duration:0)}</h1>
-                <button type="submit">Confirm</button>
+                <h1 className="text-2xl text-bold mt-2">Total Cost:{1000*(duration>=0?duration:0)}</h1>
+                <div className="flex justify-center">
+                    <button className="bg-green-800 px-2 rounded hover:bg-green-500 mt-1 font-bold text-white p-2" type="submit">Confirm</button>
+                </div>
             </form>
+            </div>
         </div>
     );
 };
--- src/pages/MeetingZones/MeetingZones.jsx
+++ src/pages/MeetingZones/MeetingZones.jsx
@@ -10,7 +10,7 @@ const MeetingZones = () => {
     },[getMeetingZones])
     return (
         <>
-            <h3 className="flex justify-center pt-6 font-serif hover:font-mono text-2xl">The capital city of Dhaka</h3>
+            <h3 className="text-center pt-6 font-serif hover:font-mono text-2xl">The capital city of Dhaka</h3>
             <h1 className="pt-4 underline underline-offset-8 pb-6 text-lg font-sans">Chose your meeting zone</h1>
             <div className="flex gap-2 flex-wrap bg-orange-100 p-4 justify-center">
                 {
