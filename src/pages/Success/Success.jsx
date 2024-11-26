1e59f21d382f95ef9009c4a58a370960e110d68f
shahadat hossen <hossantopu@gmail.com>
2024-11-26 19:19:38 +0600

    complete design for success page

--- src/pages/Success/Success.jsx
+++ src/pages/Success/Success.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Success = () => {
     return (
-        <div>
-            <h1>Success page</h1>
+        <div className='text-center pt-10'>
+            <h1 className='text-3xl font-bold'>Congratulations for the best meetup!</h1>
+            <h1 className='pt-4 text-lg'>You Have Successfully Booked Your Meeting Zone. Show <Link className='text-blue-500' to="/bookingList">Booking list <span aria-hidden="true">&rarr;</span></Link></h1>
         </div>
     );
 };
