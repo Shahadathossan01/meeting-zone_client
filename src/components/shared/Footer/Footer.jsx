2a6102d373d326843c7488ed840618ee480f4eee
shahadat hossen <hossantopu@gmail.com>
2024-11-26 19:09:58 +0600

    complete design for footer

--- src/components/shared/Footer/Footer.jsx
+++ src/components/shared/Footer/Footer.jsx
@@ -3,7 +3,10 @@
 const Footer = () => {
     return (
         <div>
-            <h1>This is footer</h1>
+            <div className="text-center bg-slate-500 pt-10 pb-10 text-white">
+                <h1>Copyright @ 2024 - All right reserved</h1>
+                <h2>S.H.Topu</h2>
+            </div>
         </div>
     );
 };
